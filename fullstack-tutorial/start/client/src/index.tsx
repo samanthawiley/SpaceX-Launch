ac0ed3ebc00b814c4bd45fdec8303a5c0862b190
Samantha Wiley <samantha.wiley@student.csulb.edu>
2020-02-19 22:38:21 -0800

    Connect client to react

--- fullstack-tutorial/start/client/src/index.tsx
+++ fullstack-tutorial/start/client/src/index.tsx
@@ -1,6 +1,12 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/react-hooks';
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import Pages from './pages';
+import injectStyles from './styles';
+
 import gql from "graphql-tag";
 
 const cache = new InMemoryCache();
@@ -12,3 +18,11 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
   link
 });
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
