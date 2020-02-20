f1f6b38a4dac42698bdac95338bd00aef644f21d
Samantha Wiley <samantha.wiley@student.csulb.edu>
2020-02-20 00:36:20 -0800

    add local resolver

--- fullstack-tutorial/start/client/src/resolvers.tsx
+++ fullstack-tutorial/start/client/src/resolvers.tsx
@@ -38,18 +38,34 @@ interface ResolverMap {
 
 interface AppResolvers extends Resolvers {
     Launch: ResolverMap;
-  }
+    Mutation: ResolverMap;
+}
   
-  export const resolvers: AppResolvers = {
+export const resolvers: AppResolvers = {
     Launch: {
       isInCart: (launch: LaunchTileTypes.LaunchTile, _, { cache }): boolean => {
-        const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems>({ 
-          query: GET_CART_ITEMS 
-        });
+        const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems>({ query: GET_CART_ITEMS });
         if (queryResult) {
           return queryResult.cartItems.includes(launch.id)
         } 
         return false;
       }
     },
+    Mutation: {
+      addOrRemoveFromCart: (_, { id }: { id: string }, { cache }): string[] => {
+        const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems>({ query: GET_CART_ITEMS });
+        if (queryResult) {
+          const { cartItems } = queryResult;
+          const data = {
+            cartItems: cartItems.includes(id)
+              ? cartItems.filter((i) => i !== id)
+              : [...cartItems, id],
+          };
+          cache.writeQuery({ query: GET_CART_ITEMS, data });
+          return data.cartItems;
+        }
+        return [];
+      },
+    },
   };
+  
